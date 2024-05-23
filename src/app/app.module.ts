c3cd6a082dca6d213f59fd27aaf31a862fe56c5b
Abdallah <bratosab@gmail.com>
2024-05-23 10:19:33 +0200

    feat(structure): project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { SharedMaterialModule } from './shared-material/shared-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedMaterialModule
   ],
   providers: [
     provideAnimationsAsync()
