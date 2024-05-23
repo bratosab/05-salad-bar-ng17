ce77d28c2a30ae91d784279944da75249ea1d49f
Abdallah <bratosab@gmail.com>
2024-05-23 15:51:56 +0200

    feat(toppings): load toppings from API

--- src/app/salad/toppings/toppings.component.ts
+++ src/app/salad/toppings/toppings.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToppingsService } from '../../services/toppings.service';
+import { Topping } from '../../models/topping.model';
 
 @Component({
   selector: 'app-toppings',
   templateUrl: './toppings.component.html',
   styleUrl: './toppings.component.scss'
 })
-export class ToppingsComponent {
+export class ToppingsComponent implements OnInit {
+  toppings: Topping[] = []
+
+  constructor(private toppingsService: ToppingsService) { }
+  
+  ngOnInit(): void {
+    this.toppingsService.getToppings().subscribe(toppings => {
+      this.toppings = toppings;
+    });
+  }
+
 
 }
